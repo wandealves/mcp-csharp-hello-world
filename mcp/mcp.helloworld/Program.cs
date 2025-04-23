6ddab91eb9cead854e7d0460098d409b365d305b
Wanderson Alves <wanderson.alves.rodrigues@gmail.com>
2025-04-22 21:34:15 -0300

    feat: criação do projeto mcp

---
+++ mcp/mcp.helloworld/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using ModelContextProtocol.Server;
+using System.ComponentModel;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Logging.AddConsole(consoleLogOptions =>
+{
+    // Configure all logs to go to stderr
+    consoleLogOptions.LogToStandardErrorThreshold = LogLevel.Trace;
+});
+builder.Services
+    .AddMcpServer()
+    .WithStdioServerTransport()
+    .WithToolsFromAssembly();
+await builder.Build().RunAsync();
+
+[McpServerToolType]
+public static class Greeting
+{
+    [McpServerTool, Description("Get a greeting message for a city")]
+    public static string GetGreeting(string city) =>
+        $"Hello! Welcome to {city}. Enjoy your stay!";
+}
